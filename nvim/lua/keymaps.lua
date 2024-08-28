a75f0c1e81de5fd2ef8dcb5e8d94e43d42c2e65f
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-28 13:32:53 +0900

    feat(neovim): add function for exchange move keys

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -4,3 +4,27 @@ local term_opts = { silent = true }
 local keymap = vim.api.nvim_set_keymap
 
 -- keymap("n", "<Leader>", "<Cmd><CR>", opts)
+
+local function set_hjkl(h, j, k, l) -- TODO: コードが冗長
+	local hu, ju, ku, lu = string.upper(h), string.upper(j), string.upper(k), string.upper(l)
+	return {
+		"h" .. h,
+		h .. "h",
+		"H" .. hu,
+		hu .. "H",
+		"j" .. j,
+		j .. "j",
+		"J" .. ju,
+		ju .. "J",
+		"k" .. k,
+		k .. "k",
+		"K" .. ku,
+		ku .. "K",
+		"l" .. l,
+		l .. "l",
+		"L" .. lu,
+		lu .. "L",
+	}
+end
+
+-- vim.opt.langmap = set_hjkl("y", "u", "i", "o")
