cd35b85c7d30231b05771e011456c1775470aea0
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-31 16:33:39 +0900

    feat(nix): add symlinks

--- home-manager/pkgs.nix
+++ home-manager/pkgs.nix
@@ -1,13 +1,19 @@
 { pkgs, ...};
-{
-  home.packages =
-  with pkgs;
-  [
-    git
-    gh
-    ghq
 
-    eza
-    ripgrep
-  ]
+let
+  git = import ./packages/git.nix;
+  neovim = import ./packages/neovim.nix;
+in {
+  home.packages =
+    with pkgs;
+    [
+      git
+      gh
+      ghq
+
+      eza
+      ripgrep
+
+      neovim
+    ]
 }
