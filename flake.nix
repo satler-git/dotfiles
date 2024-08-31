08eba4c05d1ed84ef61be6568c093fb1211f798b
satler-git <mitsclatw24@mbr.nifty.com>
2024-08-31 15:05:44 +0900

    format

--- flake.nix
+++ flake.nix
@@ -1,17 +1,19 @@
 {
-	inputs = {
-		nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-	};
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
 
-	outputs = { nixpkgs, self }:  {
-		nixosConfigurations = {
-			desktop = nixpkgs.lib.nixosSystem {
-				system = "x86_64-linux";
-				modules = [
-					./nixos/configuration.nix
-				];
-			};
-		};
-		formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
-	};
+  outputs =
+    { nixpkgs, self }:
+    {
+      nixosConfigurations = {
+        desktop = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./nixos/configuration.nix
+          ];
+        };
+      };
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-rfc-style;
+    };
 }
