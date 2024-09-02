4062f282bd968d6c99ceef960908e58a544a48ab
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-02 19:32:07 +0900

    feat(nix): support fcitx5 in wayland

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -180,7 +180,11 @@
   i18n.inputMethod = {
     enable = true;
     type = "fcitx5";
-    fcitx5.addons = [ pkgs.fcitx5-mozc ];
+    fcitx5.addons = with pkgs; [
+      fcitx5-mozc
+      fcitx5-gtk
+    ];
+    fcitx5.waylandFrontend = true;
   };
 
   fonts = {
