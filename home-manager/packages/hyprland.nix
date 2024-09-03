bcc32d6d41314ee757d2017c45792b21a85d456d
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-03 20:33:59 +0900

    feat(hypr): change (main + shift + num) to default config

--- home-manager/packages/hyprland.nix
+++ home-manager/packages/hyprland.nix
@@ -142,16 +142,16 @@
     "$mainMod, 0, workspace, 10"
 
     # Move active window to a workspace with mainMod + SHIFT + [0-9]
-    "$mainMod SHIFT, 1, focusworkspaceoncurrentmonitor, 1"
-    "$mainMod SHIFT, 2, focusworkspaceoncurrentmonitor, 2"
-    "$mainMod SHIFT, 3, focusworkspaceoncurrentmonitor, 3"
-    "$mainMod SHIFT, 4, focusworkspaceoncurrentmonitor, 4"
-    "$mainMod SHIFT, 5, focusworkspaceoncurrentmonitor, 5"
-    "$mainMod SHIFT, 6, focusworkspaceoncurrentmonitor, 6"
-    "$mainMod SHIFT, 7, focusworkspaceoncurrentmonitor, 7"
-    "$mainMod SHIFT, 8, focusworkspaceoncurrentmonitor, 8"
-    "$mainMod SHIFT, 9, focusworkspaceoncurrentmonitor, 9"
-    "$mainMod SHIFT, 0, focusworkspaceoncurrentmonitor, 10"
+    "$mainMod SHIFT, 1, movetoworkspace, 1"
+    "$mainMod SHIFT, 2, movetoworkspace, 2"
+    "$mainMod SHIFT, 3, movetoworkspace, 3"
+    "$mainMod SHIFT, 4, movetoworkspace, 4"
+    "$mainMod SHIFT, 5, movetoworkspace, 5"
+    "$mainMod SHIFT, 6, movetoworkspace, 6"
+    "$mainMod SHIFT, 7, movetoworkspace, 7"
+    "$mainMod SHIFT, 8, movetoworkspace, 8"
+    "$mainMod SHIFT, 9, movetoworkspace, 9"
+    "$mainMod SHIFT, 0, movetoworkspace, 10"
 
     # Example special workspace (scratchpad)
     "$mainMod, w, togglespecialworkspace, magic"
