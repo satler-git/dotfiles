d70a732d2dc0eb09a60b6333999f3edf19ef48ce
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-04 20:51:38 +0900

    feat(nix): use nix expr in git config(close #19)

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -1,9 +1,19 @@
 {
-  home.file = {
-    ".gitconfig".source = ../../config/git/config;
-  };
+  # home.file = {
+  #   ".gitconfig".source = ../../config/git/config;
+  # };
   programs.git = {
     enable = true;
     lfs.enable = true;
+    userEmail = "mitsclatw24@mbr.nifty.com";
+    userName = "satler-git";
+    extraConfig = {
+      core = {
+        editor = "nvim";
+      };
+      init.defaultBranch = "main";
+      credential."https://github.com".helper = "!gh auth git-credential";
+      credential."https://gist.github.com".helper = "!gh auth git-credential";
+    };
   };
 }
