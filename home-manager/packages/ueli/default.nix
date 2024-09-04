366023778b772d5a0e73bb6b0c6f2110c9057b56
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-04 18:59:40 +0900

    fix(nix): wrong url

--- home-manager/packages/ueli/default.nix
+++ home-manager/packages/ueli/default.nix
@@ -4,7 +4,7 @@ let
   version = "9.3.5";
 
   src = fetchurl {
-    url = "https://github.com/nukeop/nuclear/releases/download/v${version}/${pname}-v${version}.AppImage";
+    url = "https://github.com/oliverschwendener/ueli/releases/download/v${version}/${pname}-${version}.AppImage";
     hash = "sha256-he1uGC1M/nFcKpMM9JKY4oeexJcnzV0ZRxhTjtJz6xw=";
   };
 in
