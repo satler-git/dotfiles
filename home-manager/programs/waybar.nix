3ce70a567cce449036f1cfae6315ddaaa2c3b44a
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-06 20:20:37 +0900

    feat(hypr): add clock on waybar

--- home-manager/programs/waybar.nix
+++ home-manager/programs/waybar.nix
@@ -2,5 +2,36 @@
 {
   programs.waybar = {
     enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        modules-left = [];
+        modules-center = [ "clock" ];
+        modules-right = [ "wireplumber" ];
+
+        "clock" = {
+          timezones = "Asia/Tokyo";
+          format = "{:%H:%M}  ";
+          # tooltip = true;
+          # tooltip-format = "{calendar}";
+          # calendar = {
+          #   mode = "month";
+          #   format = {
+          #     months = "<span color='#ffead3'><b>{}</b></span>";
+          #     days = "<span color='#ecc6d9'><b>{}</b></span>";
+          #     weeks = "<span color='#99ffdd'><b>W{}</b></span>";
+          #     weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+          #     today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+          #   };
+          # };
+        };
+        "wireplumber" = {
+          format = "{volume}% {icon}";
+          format-muted = "";
+          format-icons = ["", "", ""];
+        };
+      };
+    };
   };
 }
