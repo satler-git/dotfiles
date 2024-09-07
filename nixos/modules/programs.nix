6b920745fb81db0aab4048e6358bb2045c592571
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-07 20:49:22 +0900

    feat(nix): add nix-ld

--- nixos/modules/programs.nix
+++ nixos/modules/programs.nix
@@ -3,6 +3,7 @@
     firefox.enable = true;
     git.enable = true;
     hyprland.enable = true;
+    nix-ld.enable = true;
     starship.enable = true;
     vim.enable = true;
     zsh.enable = true;
