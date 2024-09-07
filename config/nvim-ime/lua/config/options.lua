5106a583b36595fb2314bc44895562dfa75cfa22
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-07 18:35:06 +0900

    feat(neovim): add skkeleton in ime profile

--- config/nvim-ime/lua/config/options.lua
+++ config/nvim-ime/lua/config/options.lua
@@ -5,21 +5,3 @@ vim.opt.fillchars = {
 }
 vim.opt.statusline = '─'
 
-autocmd({'WinEnter', 'BufEnter', 'ColorScheme'}, {
-  group = MyAuGroup,
-  pattern = '*',
-  callback = function()
-    local bg = vim.fn.synIDattr(vim.fn.hlID("Normal"), "bg#")
-    local fg = vim.fn.synIDattr(vim.fn.hlID("VertSplit"), "fg#")
-
-    if bg ~= "" then
-      vim.cmd("hi StatusLine ctermbg=NONE guibg=" .. bg .. " ctermfg=NONE guifg=" .. fg)
-      vim.cmd("hi StatuslineNC ctermbg=NONE guibg=" .. bg .. " ctermfg=NONE guifg=" .. fg)
-    else
-      vim.cmd("hi StatusLine ctermbg=NONE guibg=NONE ctermfg=NONE guifg=" .. fg)
-      vim.cmd("hi StatuslineNC ctermbg=NONE guibg=NONE ctermfg=NONE guifg=" .. fg)
-    end
-  end,
-  once = true,
-})
-
