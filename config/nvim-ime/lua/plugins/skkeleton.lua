ba1f3a56f4e4b653954dec38e7fbf289c56b774d
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-07 19:08:37 +0900

    feat(neovim): changet to toggle from skkeleton-enable

--- config/nvim-ime/lua/plugins/skkeleton.lua
+++ config/nvim-ime/lua/plugins/skkeleton.lua
@@ -1,12 +1,14 @@
 return {
+        { "delphinus/skkeleton_indicator.nvim", opts = {} },
         {
                 "vim-skk/skkeleton",
                 event = "VeryLazy",
                 dependencies = {
                         "vim-denops/denops.vim",
+                        "delphinus/skkeleton_indicator.nvim",
                 },
                 keys = {
-                        { "<C-j>", "<Plug>(skkeleton-enable)", mode = { "i", "c" } }
+                        { "<C-j>", "<Plug>(skkeleton-toggle)", mode = { "i", "c" } }
                 },
                 config = function()
                         require("dicts")
