88f2768c889953be08cfe9b082b25f7644c85a90
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-07 15:02:42 +0900

    refactor(nix): move configration.nix to default.nix

--- flake.nix
+++ flake.nix
@@ -7,30 +7,27 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   };
 
   outputs =
     {
       nixpkgs,
-      home-manager, # hyprland,
+      home-manager,
       self,
+      ...
     }:
     {
       nixosConfigurations = {
         desktop = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           modules = [
-            ./nixos/configuration.nix
+            ./nixos
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.users.satler = import ./home-manager/linux.nix;
-              # wayland.windowManager.hyprland.enable = true;
             }
-            # hyprland.homeManagerModules.default
           ];
         };
       };
