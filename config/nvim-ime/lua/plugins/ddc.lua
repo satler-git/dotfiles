5127544d5f913b619ed26ceaa530c5f186ecde69
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-11 19:48:53 +0900

    feat(nvim-ime): change the order of call func

--- config/nvim-ime/lua/plugins/ddc.lua
+++ config/nvim-ime/lua/plugins/ddc.lua
@@ -1,39 +1,39 @@
 return {
-        {
-                "Shougo/ddc.vim",
-                lazy = false,
-                dependencies = {
-                        "vim-denops/denops.vim",
-                        "Shougo/pum.vim",
-                        "Shougo/ddc-ui-pum",
-                        "Shougo/ddc-filter-sorter_rank",
-                        "Shougo/ddc-filter-converter_remove_overlap",
+    {
+        "Shougo/ddc.vim",
+        lazy = false,
+        dependencies = {
+            "vim-denops/denops.vim",
+            "Shougo/pum.vim",
+            "Shougo/ddc-ui-pum",
+            "Shougo/ddc-filter-sorter_rank",
+            "Shougo/ddc-filter-converter_remove_overlap",
+        },
+        config = function()
+            local patch_global = vim.fn["ddc#custom#patch_global"]
+            patch_global("sourceOptions", {
+                _ = {
+                    sorters = { "sorter_rank" },
+                    converters = { "converter_remove_overlap" },
                 },
-                config = function()
-                        local patch_global = vim.fn["ddc#custom#patch_global"]
-                        patch_global('sourceOptions', {
-                                _ = {
-                                        sorters = { 'sorter_rank' },
-                                        converters = { 'converter_remove_overlap' },
-                                },
-                                ["skkeleton"] = {
-                                        mark = "skkeleton",
-                                        matchers = {},
-                                        sorters = {},
-                                        converters = {},
-                                        isVolatile = true,
-                                        minAutoCompleteLength = 1,
-                                },
-                        })
-                        patch_global('ui', 'pum')
-                        vim.fn["ddc#enable"]()
-                end,
-                keys = {
-                        { "<S-Tab>", "<Cmd>call pum#map#insert_relative(-1)<CR>", mode = "i" },
-                        { "<C-n>", "<Cmd>call pum#map#select_relative(+1)<CR>", mode = "i" },
-                        { "<C-p>", "<Cmd>call pum#map#select_relative(-1)<CR>", mode = "i" },
-                        { "<C-y>", "<Cmd>call pum#map#confirm()<CR>", mode = "i" },
-                        { "<C-e>", " <Cmd>call pum#map#cancel()<CR>", mode = "i" },
-                }
-        }
+                ["skkeleton"] = {
+                    mark = "skkeleton",
+                    matchers = {},
+                    sorters = {},
+                    converters = {},
+                    isVolatile = true,
+                    minAutoCompleteLength = 1,
+                },
+            })
+            vim.fn["ddc#enable"]()
+            patch_global("ui", "pum")
+        end,
+        keys = {
+            { "<S-Tab>", "<Cmd>call pum#map#insert_relative(-1)<CR>", mode = "i" },
+            { "<C-n>", "<Cmd>call pum#map#select_relative(+1)<CR>", mode = "i" },
+            { "<C-p>", "<Cmd>call pum#map#select_relative(-1)<CR>", mode = "i" },
+            { "<C-y>", "<Cmd>call pum#map#confirm()<CR>", mode = "i" },
+            { "<C-e>", " <Cmd>call pum#map#cancel()<CR>", mode = "i" },
+        },
+    },
 }
