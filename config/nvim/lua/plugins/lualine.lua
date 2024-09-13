a1fade5a27d773936d723209526e7e21c7a6d3f0
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-13 20:43:51 +0900

    feat(nvim): one statusline (close #69)

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,10 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = true,
+    opts = {
+        options = {
+            globalstatus = true,
+        },
+    },
     event = "VeryLazy",
 }
