b0d8ed03d743117f603acf102dd97a3da133bc87
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-15 16:05:41 +0900

    feat: git commits with signing

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -7,6 +7,10 @@
     lfs.enable = true;
     userEmail = "mitsclatw24@mbr.nifty.com";
     userName = "satler-git";
+    signing = {
+      signByDefault = true;
+      key = "97A7F4C53C4121DD";
+    };
     extraConfig = {
       core = {
         editor = "nvim";
