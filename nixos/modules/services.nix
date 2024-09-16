5f8b18d0bcef85094c004a79439dc0c2783be9af
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-16 20:43:37 +0900

    feat: add gnome to resque

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -5,6 +5,9 @@
       displayManager = {
         gdm.enable = true;
       };
+      desktopManager = {
+        gnome.enable = true;
+      };
       xkb = {
         layout = "jp";
         variant = "";
