c67c4e7f1e93369f00ec91e1ed4b1c508eb0da14
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-27 19:13:34 +0900

    feat: add some local llms(test)

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -23,6 +23,14 @@
       pulse.enable = true;
       jack.enable = true;
     };
+    ollama = {
+      enable = true;
+      acceleration = "cuda";
+      loadModels = [
+        "llama3.2"
+        "qwen2.5:14b"
+      ];
+    };
   };
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
