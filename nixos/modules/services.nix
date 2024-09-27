631262e9874060b141fbf73609873717e72c10f3
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-27 20:03:59 +0900

    feat: add llm models

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -28,7 +28,10 @@
       acceleration = "cuda";
       openFirewall = true;
       loadModels = [
+        "gemma"
         "llama3.2"
+        "phi3:14b"
+        "qwen2.5-coder"
         "qwen2.5:14b"
       ];
     };
