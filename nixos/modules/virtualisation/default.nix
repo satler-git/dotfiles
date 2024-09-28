bc9ddb8820f7845aa9ed41a5e504f6d03e2edf87
satler-git <mitsclatw24@mbr.nifty.com>
2024-09-28 19:02:17 +0900

    feat: add some virtualisation components

--- nixos/modules/virtualisation/default.nix
+++ nixos/modules/virtualisation/default.nix
@@ -6,8 +6,14 @@
     docker = {
       enable = true;
       autoPrune.enable = true;
+      enableNvidia = true;
     };
     waydroid.enable = true;
     oci-containers.backend = "docker";
+    virtualbox.host = {
+      enable = true;
+      enableKvm = true;
+      addNetworkInterface = false;
+    };
   };
 }
