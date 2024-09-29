b617388d6390a0ee6f3c82fd62303bb926fe6ac2
satler <119724672+satler-git@users.noreply.github.com>
2024-09-30 01:13:23 +0900

    feat: tab width

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -3,8 +3,10 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- タブの大きさ
--- vim.opt.tabstop = 4
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.autoindent = true
 -- vim.cmd("set retab=4")
 
 -- エンコーディング
