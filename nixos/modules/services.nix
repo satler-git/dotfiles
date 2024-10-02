facd4f03c46bf1843c34c5cb478e1a3fddcbbc46
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-02 20:52:42 +0900

    chore: update lockfile and format

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -41,12 +41,12 @@
       enable = true;
       openFirewall = true;
       environment = {
-          ANONYMIZED_TELEMETRY = "False";
-          DO_NOT_TRACK = "True";
-          SCARF_NO_ANALYTICS = "True";
-          OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
-          WEBUI_AUTH = "False";
-          PYDANTIC_SKIP_VALIDATING_CORE_SCHEMAS = "True";
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434";
+        WEBUI_AUTH = "False";
+        PYDANTIC_SKIP_VALIDATING_CORE_SCHEMAS = "True";
       };
     };
     hoogle = {
