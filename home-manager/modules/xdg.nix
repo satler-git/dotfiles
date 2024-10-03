9dae52a844dc36ab507532ea6181aad251dc9a73
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-03 19:58:03 +0900

    feat: add xdg-desktop-portal and neovim nightly

---
+++ home-manager/modules/xdg.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+{
+  xdg = {
+    portal = {
+      enable = true;
+      configPackages = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal
+      ];
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal
+      ];
+      xdgOpenUsePortal = true;
+    };
+  };
+}
