f13e0c2e1213fc8bc3984f6d377a3e70a63be420
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-05 13:35:45 +0900

    feat: use nvfetcher(close #101)

--- home-manager/programs/neovim.nix
+++ home-manager/programs/neovim.nix
@@ -1,18 +1,14 @@
 { pkgs, ... }:
 let
-  harper_ls = pkgs.rustPlatform.buildRustPackage {
+  harper-src = (pkgs.callPackages ../../_sources/generated.nix {}).harper-ls;
+  harper-ls = pkgs.rustPlatform.buildRustPackage {
     pname = "harper-ls";
     version = "0.11.0";
 
-    src = pkgs.fetchFromGitHub {
-      owner = "elijah-potter";
-      repo = "harper";
-      rev = "3c06fc8b763835a16885c104b69e461f8870bb1e";
-      sha256 = "sha256-sDXwsv5g32SXfZbv8IX/r3hj1iOPOaWbCJxEe/oxZ8Y=";
-    };
-
-    cargoHash = "sha256-9HSXL6llRNolH4tSvYKqCFcaTwpuyr+h3wzjzz7QLZM=";
+    src = harper-src.src;
+    cargoLock.lockFile = harper-src.cargoLock."Cargo.lock".lockFile;
   };
+
   dicts_lua = ''
     -- SKKELETON's JISYO
     vim.api.nvim_exec(
@@ -47,7 +43,7 @@ in
       nil
       taplo
       yaml-language-server
-      harper_ls
+      harper-ls
     ];
     extraLuaPackages = ps: [ ps.jsregexp ];
   };
