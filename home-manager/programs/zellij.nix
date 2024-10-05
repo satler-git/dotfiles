3c5a1190c13d4f764e78124a6adc3c9b255e8a82
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-05 14:51:32 +0900

    feat: add some plugins to zellij

--- home-manager/programs/zellij.nix
+++ home-manager/programs/zellij.nix
@@ -1,10 +1,20 @@
-{
+{ pkgs, ... }:
+let
+  sources = (pkgs.callPackages ../../_sources/generated.nix {});
+  zellij-room = sources.zellij-room;
+  zellij-zj-quit = sources.zellij-zj-quit;
+in{
   programs.zellij = {
     enable = true;
     enableZshIntegration = true;
   };
   home.file.zellij = {
     target = ".config/zellij/config.kdl";
-    source = ../../config/zellij/config.kdl;
+    source = pkgs.substituteAll {
+      src = ../../config/zellij/config.kdl;
+      zj-quit_location = "${zellij-zj-quit.src}";
+      room_location = "${zellij-room.src}";
+      zjstatus_location = "${pkgs.zjstatus}/bin/zjstatus.wasm";
+    };
   };
 }
