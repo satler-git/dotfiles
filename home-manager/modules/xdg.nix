1e180619f363123d4637ec36a68ddaf4a1eb543b
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-06 11:48:07 +0900

    fix: add configPackages

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -3,10 +3,10 @@
   xdg = {
     portal = {
       enable = true;
-#      configPackages = with pkgs; [
-#        xdg-desktop-portal-hyprland
-#        xdg-desktop-portal
-#      ];
+     configPackages = with pkgs; [
+       xdg-desktop-portal-hyprland
+       xdg-desktop-portal
+     ];
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
         xdg-desktop-portal-hyprland
