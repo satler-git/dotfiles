229c4812131316eca870207831b461a135cfcacd
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-06 14:32:25 +0900

    remove other xdg desktop portals

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -4,13 +4,14 @@
     portal = {
       enable = true;
      configPackages = with pkgs; [
+       # xdg-desktop-portal-gtk
        xdg-desktop-portal-hyprland
-       xdg-desktop-portal
+       # xdg-desktop-portal
      ];
       extraPortals = with pkgs; [
-        xdg-desktop-portal-gtk
+        # xdg-desktop-portal-gtk
         xdg-desktop-portal-hyprland
-        xdg-desktop-portal
+        # xdg-desktop-portal
       ];
       xdgOpenUsePortal = true;
     };
