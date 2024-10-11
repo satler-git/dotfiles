b8c365184b315009253a6a80b70631a90fedb932
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-11 19:38:29 +0900

    feat: add cachix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -6,6 +6,12 @@
         "nix-command"
         "flakes"
       ];
+      substituters = [
+        "https://nix-community.cachix.org"
+      ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
     gc = {
       automatic = true;
