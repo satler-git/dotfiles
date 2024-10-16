8f4a889ebcca828079060543a13fd78c8c3cc297
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-16 20:18:51 +0900

    :sparkles::recycle: use oil instead to neo-tree

--- config/nvim/lua/plugins/ui/tree.lua
+++ config/nvim/lua/plugins/ui/tree.lua
@@ -1,23 +1,10 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  cmd = {
-    "Neotree",
-  },
+  "stevearc/oil.nvim",
+  opts = {},
   keys = {
-    { "<C-n>", "<Cmd>Neotree filesystem reveal left toggle<CR>" },
+    { "<C-n>", "<Cmd>Oil<CR>" },
   },
-  opts = {
-    enable_git_status = true,
-    filesystem = {
-      window = {
-        width = 30, -- ここで幅を設定します。数値は任意で変更可能です。
-      },
-    },
-  },
+  cmd = { "Oil" },
+  -- Optional dependencies
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
