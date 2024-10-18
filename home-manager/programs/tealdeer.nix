b1c23d3c816a0d5fd9aad025e3a5c0db04638ac6
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-18 19:35:13 +0900

    :sparkles: add style settings for tealdeer

--- home-manager/programs/tealdeer.nix
+++ home-manager/programs/tealdeer.nix
@@ -2,6 +2,15 @@
   programs.tealdeer = {
     enable = true;
     settings = {
+      style = {
+        foreground = "cyan";
+        example_text = {
+          foreground = "blue";
+        };
+        command_name = {
+          foreground = "green";
+        };
+      };
       updates = {
         auto_update = true;
       };
