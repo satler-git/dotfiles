b0c8aa8a29c5a4ac5bf71a9419b5655344e07271
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-24 20:19:57 +0900

    :sparkles: config podman

--- nixos/modules/virtualisation/default.nix
+++ nixos/modules/virtualisation/default.nix
@@ -1,37 +1,24 @@
 {
   imports = [
-    ./grimoire.nix
+    # ./grimoire.nix
   ];
   virtualisation = {
-    # docker = {
-    #   enable = true;
-    #   autoPrune.enable = true;
-    # };
     containers.enable = true;
     podman = {
       enable = true;
 
+      # For compatibles with
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
+      dockerSocket.enable = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
+
+      autoPrune.enable = true;
     };
     waydroid.enable = true;
     oci-containers.backend = "podman";
     libvirtd.enable = true;
-    # vmware.host = {
-    #   enable = true;
-    #   extraConfig = ''
-    #     mks.gl.allowUnsupportedDrivers = "TRUE"
-    #     mks.vk.allowUnsupportedDevices = "TRUE"
-    #   '';
-    # };
-    # TODO: i3とかX上じゃないとだめそう
-    # virtualbox.host = {
-    #   enable = true;
-    #   enableKvm = true;
-    #   addNetworkInterface = false;
-    # };
   };
 }
