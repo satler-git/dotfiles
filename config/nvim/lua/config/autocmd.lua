948a872642c08cbb5ff95154534b06a74b87a3dc
satler-git <mitsclatw24@mbr.nifty.com>
2024-10-29 19:34:42 +0900

    Nixのインデントの大きさを変える

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -1,5 +1,13 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
+  callback = function(_args)
     vim.lsp.inlay_hint.enable(true)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "nix",
+  callback = function(_)
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
   end,
 })
