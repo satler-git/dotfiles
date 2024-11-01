3a2993ac6b12d7887a9c5a4ff6abda673244b3da
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-01 20:18:19 +0900

    :bug: hardtimeが起動されなくてeasytime

--- config/nvim/lua/plugins/misc/hardtime.lua
+++ config/nvim/lua/plugins/misc/hardtime.lua
@@ -1,7 +1,9 @@
 return {
   {
     "m4xshen/hardtime.nvim",
+    event = "VeryLazy",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    cmd = { "Hardtime" },
     opts = {},
   },
 }
