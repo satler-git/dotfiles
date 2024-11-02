fe098cd2a27b681340b0b68609830c7e3d926165
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-02 13:19:38 +0900

    :wastebasket: remove an unnecessary package(close #167)

--- nixos/modules/environment.nix
+++ nixos/modules/environment.nix
@@ -5,7 +5,6 @@
     systemPackages = with pkgs; [
       dropbox-cli
       egl-wayland # TODO: maybe Nvidia here
-      nixfmt-rfc-style
       nvidia-vaapi-driver # TODO: Nvidia here
       polkit_gnome
     ];
