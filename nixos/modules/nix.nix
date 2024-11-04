8f5b0c4a97cb5effc52444d719d64659093592ab
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-04 19:13:42 +0900

    :sparkles: add miso-haskell cachix

--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -8,9 +8,11 @@
       ];
       substituters = [
         "https://nix-community.cachix.org"
+        "https://miso-haskell.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
       ];
     };
     gc = {
