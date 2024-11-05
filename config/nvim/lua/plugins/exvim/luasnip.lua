ba96ce38036ab9b4fa20e2d00f914617a1fa0d8b
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-05 11:20:57 +0900

    :sparkles: add haskell-snipetts

--- config/nvim/lua/plugins/exvim/luasnip.lua
+++ config/nvim/lua/plugins/exvim/luasnip.lua
@@ -1,11 +1,18 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
+    "L3MON4D3/LuaSnip", -- nvim-cmpからロードされる
+    dependencies = {
+      "mrcjkb/haskell-snippets.nvim",
+    },
     version = "v2.*",
     lazy = true,
     -- build = "make install_jsregexp",
     config = function()
       require("luasnip.loaders.from_lua").load()
+
+      -- Load Haskell snipetts
+      local haskell_snippets = require("haskell-snippets").all
+      require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
     end,
   },
 }
