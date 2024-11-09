16cb778803960d4c54b9f49a8b6d8e7bbe938426
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-09 20:45:18 +0900

    :sparkles: add aerial.nvim(close #176)

--- config/nvim/lua/plugins/ui/lualine.lua
+++ config/nvim/lua/plugins/ui/lualine.lua
@@ -5,6 +5,14 @@ return {
     options = {
       globalstatus = true,
     },
+    sections = {
+      lualine_a = { "mode" },
+      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_c = { "filename" },
+      lualine_x = { "encoding", "fileformat", "filetype" },
+      lualine_y = { "progress" },
+      lualine_z = { "location" },
+    },
   },
   event = "VeryLazy",
 }
