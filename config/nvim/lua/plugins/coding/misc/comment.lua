4d51156bcaa1c5b89ef4c87ccc886768cfc7a815
satler <119724672+satler-git@users.noreply.github.com>
2024-11-13 12:22:01 +0900

    :sparkles: add `JoosepAlviste/nvim-ts-context-commentstring`

--- config/nvim/lua/plugins/coding/misc/comment.lua
+++ config/nvim/lua/plugins/coding/misc/comment.lua
@@ -1,9 +1,24 @@
 return {
-  "numToStr/Comment.nvim",
-  config = true,
-  keys = {
-    { "gc" },
-    { "gc", mode = "x" },
+  { 
+    "numToStr/Comment.nvim",
+    keys = {
+      { 'gc', mode = { 'n', 'x' } },
+      { 'gb', mode = { 'n', 'x' } },
+      { 'gcc', mode = 'n' },
+    },
+    dependencies = {
+      "JoosepAlviste/nvim-ts-context-commentstring"
+    },
+    config = function()
+      require('Comment').setup {
+        pre_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook(),
+      }
+    end,
   },
-  -- event = "VeryLazy",
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    opts = {
+        enable_autocmd = false,
+    },
+  },
 }
