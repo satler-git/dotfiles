1034f0f89c6c9ceea146b21d7c4eaf38b3366bf8
satler <119724672+satler-git@users.noreply.github.com>
2024-11-18 01:01:17 +0900

    :recycle: refactor downloading wallpaper with `pkgs.nixos-artwork`

--- home-manager/stylix.nix
+++ home-manager/stylix.nix
@@ -6,10 +6,10 @@ in
   stylix = {
     enable = true;
 
-    image = pkgs.fetchurl {
-      url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/ea1384e183f556a94df85c7aa1dcd411f5a69646/wallpapers/nix-wallpaper-nineish.png";
-      hash = "sha256-EMSD1XQLaqHs0NbLY0lS1oZ4rKznO+h9XOGDS121m9c=";
-    };
+    image = let
+        wallpaper = name: "${pkgs.nixos-artwork.wallpapers.${name}}/share/backgrounds/nixos/nix-wallpaper-${name}.png";
+      in
+        wallpaper "nineish";
 
     cursor.package = pkgs.bibata-cursors;
     cursor.name = "Bibata-Modern-Ice";
