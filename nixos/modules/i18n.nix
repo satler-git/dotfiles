abc3743ce4ade1bd0196ec7dfe6bd08b0a37ad5c
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-18 20:51:16 +0900

    :sparkles: add fcitx5 addons

--- nixos/modules/i18n.nix
+++ nixos/modules/i18n.nix
@@ -21,8 +21,11 @@
     enable = true;
     type = "fcitx5";
     fcitx5.addons = with pkgs; [
-      fcitx5-mozc
+      # fcitx5-mozc
       fcitx5-gtk
+      # fcitx5-mozc-ut
+      fcitx5-skk
+      fcitx5-tokyonight
     ];
     fcitx5.waylandFrontend = true;
   };
