c9e3869c2c51a16be7c0b0da48356176a681fd78
satler <119724672+satler-git@users.noreply.github.com>
2024-11-22 08:59:55 +0900

    remove a temporary-fix overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,14 +3,12 @@
   stable = final: prev: {
     inherit (inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system})
       # fcitx5-mozc
-      # mozc
       ;
   };
 
   temporary-fix = final: prev: {
     inherit (inputs.nixpkgs-unstable-small.legacyPackages.${final.stdenv.hostPlatform.system})
-      # https://nixpk.gs/pr-tracker.html?pr=356590
-      fcitx5-mozc
+      # fcitx5-mozc
       ;
   };
 }
