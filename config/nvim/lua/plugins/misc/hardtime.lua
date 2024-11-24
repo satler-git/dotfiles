50010a293233d10574f4f71151ef2dfe94318846
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-24 15:15:04 +0900

    :wastebasket: disable hardtime.nvim

--- config/nvim/lua/plugins/misc/hardtime.lua
+++ config/nvim/lua/plugins/misc/hardtime.lua
@@ -1,6 +1,7 @@
 return {
   {
     "m4xshen/hardtime.nvim",
+    enable = false,
     event = "VeryLazy",
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     cmd = { "Hardtime" },
