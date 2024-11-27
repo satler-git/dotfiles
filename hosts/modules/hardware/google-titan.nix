ff21f3799548f05e0b3cc9f04a15167d46721473
satler <119724672+satler-git@users.noreply.github.com>
2024-11-27 12:07:00 +0900

    :recycle: use `lib.mkBefore` to concat string

--- hosts/modules/hardware/google-titan.nix
+++ hosts/modules/hardware/google-titan.nix
@@ -1,7 +1,8 @@
+{ lib, ... }:
 {
   # Add support for google titan sec key
   # See also https://support.google.com/titansecuritykey/answer/9148044?hl=ja for more info
-  services.udev.extraRules = ''
+  services.udev.extraRules = lib.mkBefore ''
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", ATTRS{idVendor}=="18d1|096e", ATTRS{idProduct}=="5026|0858|085b", TAG+="uaccess"
   '';
 }
--- hosts/modules/hardware/microbit.nix
+++ hosts/modules/hardware/microbit.nix
@@ -1,5 +1,6 @@
+{ lib, ... }:
 {
-  services.udev.extraRules = ''
+  services.udev.extraRules = lib.mkBefore ''
     KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev"
   '';
 
