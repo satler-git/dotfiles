78ca932dc9af9d5b50afe12348f2785de18d544e
satler <119724672+satler-git@users.noreply.github.com>
2024-11-28 22:52:21 +0900

    :sparkles: add `nixConfig` in toplevel(close #218)

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "My dotfiles for Nixos";
+  description = "My dotfiles for NixOS";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -44,6 +44,20 @@
       inputs.flake-parts.follows = "neovim-nightly-overlay/flake-parts";
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
+  };
+
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+      "https://miso-haskell.cachix.org"
+      "https://numtide.cachix.org"
+    ];
+
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+    ];
   };
 
   outputs =
--- nixos/modules/nix.nix
+++ nixos/modules/nix.nix
@@ -9,10 +9,12 @@
       substituters = [
         "https://nix-community.cachix.org"
         "https://miso-haskell.cachix.org"
+        "https://numtide.cachix.org"
       ];
       trusted-public-keys = [
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "miso-haskell.cachix.org-1:6N2DooyFlZOHUfJtAx1Q09H0P5XXYzoxxQYiwn6W1e8="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
       ];
     };
     gc = {
