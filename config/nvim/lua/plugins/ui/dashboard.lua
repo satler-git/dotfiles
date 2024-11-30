d0f0f9fac6d0824f5edb8e44a8db153fab538a5c
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-30 17:44:05 +0900

    :sparkles: add alpha.nvim(#149)

---
+++ config/nvim/lua/plugins/ui/dashboard.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "goolord/alpha-nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "nvim-lua/plenary.nvim",
+    },
+    event = "VimEnter",
+    config = function()
+      -- local dashboard = require("alpha.themes.theta")
+      local dashboard = require("alpha.themes.dashboard")
+      -- dashboard.file_icons.provider = "devicons"
+
+      dashboard.section.header.val = {
+        [[                               __                ]],
+        [[  ___     ___    ___   __  __ /\_\    ___ ___    ]],
+        [[ / _ `\  / __`\ / __`\/\ \/\ \\/\ \  / __` __`\  ]],
+        [[/\ \/\ \/\  __//\ \_\ \ \ \_/ |\ \ \/\ \/\ \/\ \ ]],
+        [[\ \_\ \_\ \____\ \____/\ \___/  \ \_\ \_\ \_\ \_\]],
+        [[ \/_/\/_/\/____/\/___/  \/__/    \/_/\/_/\/_/\/_/]],
+      }
+
+      require("alpha").setup(dashboard.config)
+    end,
+  },
+}
