71083fa3481c44fa21a993c92603cca690b69b67
satler-git <mitsclatw24@mbr.nifty.com>
2024-11-30 17:25:48 +0900

    :sparkles: use kanagawa(close #211)

--- config/nvim/lua/plugins/ui/theme.lua
+++ config/nvim/lua/plugins/ui/theme.lua
@@ -7,6 +7,16 @@ return {
       vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
       vim.cmd([[colorscheme aura-soft-dark-soft-text]])
     end,
+    enabled = false,
+  },
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("kanagawa").setup({})
+      vim.cmd([[colorscheme kanagawa]])
+    end,
     enabled = true,
   },
 }
