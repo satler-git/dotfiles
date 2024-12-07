7ba839687495fc951d73ccfe6d881e0b495c54d2
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-07 17:40:29 +0900

    :recycle: lazy load haskell-sinpetts

--- config/nvim/lua/plugins/coding/lang/haskell.lua
+++ config/nvim/lua/plugins/coding/lang/haskell.lua
@@ -1,12 +1,28 @@
+local ft = { "haskell", "lhaskell", "cabal", "cabalproject", "tidal" }
+
 return {
   {
     "mrcjkb/haskell-tools.nvim",
     version = "^4",
     -- lazy = false,
-    ft = { "haskell", "lhaskell", "cabal", "cabalproject", "tidal" },
+    ft = ft,
     config = function()
       local ht = require("haskell-tools")
-      vim.keymap.set("n", "K", "<Cmd>require('haskell-tools').hoogle.hoogle_signature()<CR>")
+
+      vim.keymap.set("n", "K", function()
+        ht.hoogle.hoogle_signature()
+      end)
+    end,
+  },
+  {
+    "mrcjkb/haskell-snippets.nvim",
+    dependencies = {
+      "L3MON4D3/LuaSnip",
+    },
+    ft = ft,
+    config = function()
+      local haskell_snippets = require("haskell-snippets").all
+      require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
     end,
   },
 }
--- config/nvim/lua/plugins/exvim/luasnip.lua
+++ config/nvim/lua/plugins/exvim/luasnip.lua
@@ -1,18 +1,11 @@
 return {
   {
     "L3MON4D3/LuaSnip", -- nvim-cmpからロードされる
-    dependencies = {
-      "mrcjkb/haskell-snippets.nvim",
-    },
     version = "v2.*",
     lazy = true,
     -- build = "make install_jsregexp",
     config = function()
-      require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/luasnippets/" })
-
-      -- Load Haskell snipetts
-      local haskell_snippets = require("haskell-snippets").all
-      require("luasnip").add_snippets("haskell", haskell_snippets, { key = "haskell" })
+      require("luasnip.loaders.from_lua").load({ paths = { "~/.config/nvim/luasnippets/" } })
     end,
   },
 }
