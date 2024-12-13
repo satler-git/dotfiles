d681067ba9c67f799790f9f789ecd908ee7166a3
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-13 20:05:29 +0900

    feat: boot(close #200)

--- nixos/modules/boot.nix
+++ nixos/modules/boot.nix
@@ -1,8 +1,32 @@
 { pkgs, ... }:
 {
   boot = {
-    loader.systemd-boot.enable = true;
-    loader.efi.canTouchEfiVariables = true;
     kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+
+    consoleLogLevel = 0;
+
+    kernelParams = [
+      "quiet"
+      "udev.log_level=3"
+    ];
+    initrd.verbose = false;
+    loader = {
+      systemd-boot = {
+        enable = true;
+        editor = false;
+        configurationLimit = 5;
+      };
+      timeout = 3;
+      efi.canTouchEfiVariables = true;
+    };
+    plymouth = {
+      enable = true;
+      themePackages = with pkgs; [
+        (catppuccin-plymouth.override {
+          variant = "frappe";
+        })
+      ];
+      theme = "catppuccin-frappe";
+    };
   };
 }
