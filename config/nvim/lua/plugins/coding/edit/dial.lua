a8a7d782004b5a31858031a56b8200e8ea829a73
satler <119724672+satler-git@users.noreply.github.com>
2024-12-19 14:26:11 +0900

    feat: add dial.nvim mappings

--- config/nvim/lua/plugins/coding/edit/dial.lua
+++ config/nvim/lua/plugins/coding/edit/dial.lua
@@ -3,5 +3,11 @@ return {
   keys = {
     { "<C-a>", "<Plug>(dial-increment)" },
     { "<C-x>", "<Plug>(dial-decrement)" },
+    { "g<C-a>", "g<Plug>(dial-increment)" },
+    { "g<C-x>", "g<Plug>(dial-decrement)" },
+    { "<C-a>", "<Plug>(dial-increment)", mode = "v" },
+    { "<C-x>", "<Plug>(dial-decrement)", mode  = "v" },
+    { "g<C-a>", "g<Plug>(dial-increment)", mode = "v" },
+    { "g<C-x>", "g<Plug>(dial-decrement)", mode = "v" },
   },
 }
