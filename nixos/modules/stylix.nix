d393169a6e88587993ecc3b74f2ee56e1a5e085a
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-22 16:27:21 +0900

    refactor: remove an outdated comment

--- nixos/modules/stylix.nix
+++ nixos/modules/stylix.nix
@@ -9,14 +9,6 @@ in
 
     enable = false;
 
-    # image = pkgs.fetchurl {
-    #     url = "https://raw.githubusercontent.com/NixOS/nixos-artwork/ea1384e183f556a94df85c7aa1dcd411f5a69646/wallpapers/nix-wallpaper-nineish.png";
-    #     hash = "sha256-EMSD1XQLaqHs0NbLY0lS1oZ4rKznO+h9XOGDS121m9c=";
-    # };
-
-    # cursor.package = pkgs.bibata-cursors;
-    # cursor.name = "Bibata-Modern-Ice";
-    #
     base16Scheme = "${pkgs.base16-schemes}/share/themes/${theme}.yaml";
   };
 }
