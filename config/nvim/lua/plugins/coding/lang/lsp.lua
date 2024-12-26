fef67848e1839b607e27cd3f5428a4450e74b00a
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-26 20:25:12 +0900

    feat: add neoconf

--- config/nvim/lua/plugins/coding/lang/lsp.lua
+++ config/nvim/lua/plugins/coding/lang/lsp.lua
@@ -1,48 +1,56 @@
 return {
-  "neovim/nvim-lspconfig",
-  event = { "BufReadPost", "BufWritePost", "BufNewFile" },
-  dependencies = {
-    {
-      "hrsh7th/cmp-nvim-lsp",
-      "b0o/schemastore.nvim",
-      "j-hui/fidget.nvim",
+  {
+    "neovim/nvim-lspconfig",
+    event = { "BufReadPost", "BufWritePost", "BufNewFile" },
+    dependencies = {
       {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
-        config = function()
-          require("lsp_lines").setup()
-          vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
-        end,
-      },
-      {
-        "nvimdev/lspsaga.nvim",
-        dependencies = {
-          "nvim-treesitter/nvim-treesitter",
-          "nvim-tree/nvim-web-devicons",
+        "folke/neoconf.nvim",
+        "hrsh7th/cmp-nvim-lsp",
+        "b0o/schemastore.nvim",
+        "j-hui/fidget.nvim",
+        {
+          "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+          config = function()
+            require("lsp_lines").setup()
+            vim.keymap.set("", "<Leader>l", require("lsp_lines").toggle, { desc = "Toggle lsp_lines" })
+          end,
         },
-        keys = {
-          { "K", "<Cmd>Lspsaga hover_doc<CR>", desc = "Show the hover document" },
-          { "ga", "<Cmd>Lspsaga code_action<CR>", desc = "Show and Run code actions" },
-          { "gn", "<Cmd>Lspsaga rename<CR>", desc = "Rename identifier" },
-          { "gd", "<Cmd>Lspsaga peek_definition<CR>" },
-          { "gr", "<Cmd>Lspsaga finder<CR>" },
-
-          { "ge", "<Cmd>Lspsaga show_line_diagnostics<CR>" },
-          { "[e", "<Cmd>Lspsaga diagnostic_jump_next<CR>" },
-          { "]e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>" },
-          { "<Space><C-o>", "<Cmd>Lspsaga outline<CR>" },
-        },
-        opts = {
-          symbol_in_winbar = {
-            enable = true,
+        {
+          "nvimdev/lspsaga.nvim",
+          dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-tree/nvim-web-devicons",
           },
-          lightbulb = {
-            enable = false,
+          keys = {
+            { "K", "<Cmd>Lspsaga hover_doc<CR>", desc = "Show the hover document" },
+            { "ga", "<Cmd>Lspsaga code_action<CR>", desc = "Show and Run code actions" },
+            { "gn", "<Cmd>Lspsaga rename<CR>", desc = "Rename identifier" },
+            { "gd", "<Cmd>Lspsaga peek_definition<CR>" },
+            { "gr", "<Cmd>Lspsaga finder<CR>" },
+
+            { "ge", "<Cmd>Lspsaga show_line_diagnostics<CR>" },
+            { "[e", "<Cmd>Lspsaga diagnostic_jump_next<CR>" },
+            { "]e", "<Cmd>Lspsaga diagnostic_jump_prev<CR>" },
+            { "<Space><C-o>", "<Cmd>Lspsaga outline<CR>" },
+          },
+          opts = {
+            symbol_in_winbar = {
+              enable = true,
+            },
+            lightbulb = {
+              enable = false,
+            },
           },
         },
       },
     },
+    config = function()
+      require("config.ls")
+    end,
   },
-  config = function()
-    require("config.ls")
-  end,
+  {
+    "folke/neoconf.nvim",
+    opts = {},
+    cmd = { "Neoconf" },
+  },
 }
