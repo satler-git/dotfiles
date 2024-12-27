38989c8c49270fccdd957d60a124c40acfe6dd6b
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-27 20:18:18 +0900

    feat: add theme of ghostty

--- home-manager/programs/ghostty.nix
+++ home-manager/programs/ghostty.nix
@@ -2,4 +2,7 @@
   xdg.configFile."ghostty/config" = {
     source = ../../config/ghostty/config;
   };
+  xdg.configFile."ghostty/themes/aura-soft-dark-soft-text" = {
+    source = ../../config/ghostty/theme-aura-sd-st;
+  };
 }
