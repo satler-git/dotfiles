ab2784c33440751c6ddab910293b1b98cb0d9c27
satler-git <mitsclatw24@mbr.nifty.com>
2024-12-29 21:31:49 +0900

    feat: made gtk apps happy
    Quoted from https://github.com/nydragon/nix-da/blob/e0c1e40f27d53c78744dcef76afd415039a405af/modules/portals.nix#L16

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 {
   xdg = {
+    mimeApps = {
+      enable = true;
+    };
     portal = {
       enable = true;
       configPackages = with pkgs; [
         # xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        xdg-desktop-portal-hyprland # ここではHyprland上ではxdg-desktop-portal-hyprlandのシェアピッカーを使うという設定とその実装をそれぞれ使っている
         # xdg-desktop-portal
       ];
       extraPortals = with pkgs; [
-        # xdg-desktop-portal-gtk
+        xdg-desktop-portal-gtk
         xdg-desktop-portal-hyprland
-        # xdg-desktop-portal
+        xdg-desktop-portal
       ];
       xdgOpenUsePortal = true;
     };
