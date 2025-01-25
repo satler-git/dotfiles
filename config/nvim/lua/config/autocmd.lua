35e9b3be9ab18c973437f1ff2b8ec86e825599a1
satler <satler@satler.dev>
2025-01-25 11:42:48 +0900

    refactor: create local function

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -1,21 +1,19 @@
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(_args)
+  callback = function(_)
     vim.lsp.inlay_hint.enable(true)
   end,
 })
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "nix",
-  callback = function(_)
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-  end,
-})
+local function two_space(pattern)
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = pattern,
+    callback = function(_)
+      vim.opt_local.shiftwidth = 2
+      vim.opt_local.tabstop = 2
+    end,
+  })
+end
 
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "json",
-  callback = function(_)
-    vim.opt_local.shiftwidth = 2
-    vim.opt_local.tabstop = 2
-  end,
-})
+two_space("nix")
+two_space("json")
+two_space("sh")
