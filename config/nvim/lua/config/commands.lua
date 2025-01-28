7a7499f0663de285c49c403d04bc47880f03dc91
satler <satler@satler.dev>
2025-01-28 19:28:06 +0900

    feat:

--- config/nvim/lua/config/commands.lua
+++ config/nvim/lua/config/commands.lua
@@ -2,3 +2,22 @@
 vim.api.nvim_create_user_command("ToggleInlayHint", function()
   vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, {})
+
+vim.api.nvim_create_user_command("CopyRish", function()
+  vim.system({ "pandoc", "--self-contained", "-t", "html", "-f", "markdown_github+raw_html" }, {
+    stdin = vim.api.nvim_buf_get_text(0, 0, 0, -1, -1, {}),
+  }, function(pandoc_result)
+    if pandoc_result.code ~= 0 then
+      vim.notify("Pandoc Error", vim.log.levels.ERROR)
+      return
+    end
+
+    vim.system({ "wl-copy", "--type", "text/html" }, {
+      stdin = pandoc_result.stdout,
+    }, function(copy_result)
+      if copy_result.code == 0 then
+        vim.notify("Copied!", vim.log.levels.INFO)
+      end
+    end)
+  end)
+end, {})
