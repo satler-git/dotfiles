a99b633f96b9e60729097a569d49cee9fdee2c8c
satler <satler@satler.dev>
2025-02-10 22:15:46 +0900

    feat: hujson

--- config/nvim/lua/config/autocmd.lua
+++ config/nvim/lua/config/autocmd.lua
@@ -14,6 +14,17 @@ local function two_space(pattern)
   })
 end
 
+local function set_ft(pattern, filetype)
+  vim.api.nvim_create_autocmd("BufReadPre", {
+    pattern = pattern,
+    callback = function(_)
+      vim.opt_local.filetype = filetype
+    end,
+  })
+end
+
 two_space("nix")
 two_space("json")
 two_space("sh")
+
+set_ft("hujson", "json5")
