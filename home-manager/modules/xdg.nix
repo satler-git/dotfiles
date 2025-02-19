6b3fe45654d96c6ac179860fb21a888e60ef57c9
satler <satler@satler.dev>
2025-02-19 20:04:17 +0900

    feat: config xdg

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -11,9 +11,26 @@
         "x-scheme-handler/https" = "firefox.desktop";
         "text/html" = "firefox.desktop";
         "application/pdf" = "firefox.desktop";
-        "image/*" = "nomacs.desktop";
-        "audio/*" = "mpv.desktop";
-        "video/*" = "mpv.desktop";
+
+        "image/bmp" = "org.nomacs.ImageLounge.desktop";
+        "image/gif" = "org.nomacs.ImageLounge.desktop";
+        "image/jpeg" = "org.nomacs.ImageLounge.desktop";
+        "image/png" = "org.nomacs.ImageLounge.desktop";
+        "image/webp" = "org.nomacs.ImageLounge.desktop";
+
+        "audio/aac" = "mpv.desktop";
+        "audio/midi" = "mpv.desktop";
+        "audio/mpeg" = "mpv.desktop";
+        "audio/ogg" = "mpv.desktop";
+        "audio/opus" = "mpv.desktop";
+        "audio/wav" = "mpv.desktop";
+        "audio/webm" = "mpv.desktop";
+
+        "video/mpeg" = "mpv.desktop";
+        "video/mp4" = "mpv.desktop";
+        "video/ogg" = "mpv.desktop";
+        "video/webm" = "mpv.desktop";
+        "video/x-msvideo" = "mpv.desktop";
       };
     };
     portal = {
