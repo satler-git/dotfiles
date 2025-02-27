e9e3cded5bb4ade737e1f2f068868bf5930fda31
satler <satler@satler.dev>
2025-02-27 19:24:59 +0900

    feat: disable nvim-insx sometimes

--- config/nvim/lua/plugins/coding/edit/pair-brackets.lua
+++ config/nvim/lua/plugins/coding/edit/pair-brackets.lua
@@ -10,6 +10,46 @@ return {
     event = "InsertEnter",
     config = function()
       require("insx.preset.standard").setup()
+
+      local insx = require("insx")
+
+      insx.add(
+        "'",
+        insx.with({
+          enabled = function(ctx)
+            local before_borrowing = ctx.match([=[&\%#]=])
+            local in_type_parameters = false
+
+            local node = vim.treesitter.get_node({ pos = { ctx.row(), ctx.col() } })
+
+            if not node then
+              return true
+            end
+
+            while node do
+              if
+                vim.tbl_contains({
+                  "trait_bounds",
+                  "type_arguments",
+                  "type_parameters",
+                }, node:type())
+              then
+                in_type_parameters = true
+                break
+              end
+              node = node:parent()
+            end
+
+            return in_type_parameters or before_borrowing
+          end,
+          action = function(ctx)
+            ctx.send("'")
+          end,
+          priority = 1000,
+        }, {
+          insx.with.filetype({ "rust" }),
+        })
+      )
     end,
   },
 }
