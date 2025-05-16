933ff7b2ccdf6500b60c33b2b9e5e37a46ad2498
satler <satler@satler.dev>
2025-05-16 21:15:35 +0900

    feat: take a screen capture from yurf

--- home-manager/programs/yurf.nix
+++ home-manager/programs/yurf.nix
@@ -1,4 +1,7 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
+let
+  writeShellScript = name: content: builtins.toString (pkgs.writeShellScript name content);
+in
 {
   imports = [
     inputs.yurf.homeManagerModules.yurf
@@ -15,6 +18,14 @@
       {
         name = "hyprsunset: sunset";
         command = "hyprctl hyprsunset temperature 2500";
+      }
+      {
+        name = "Take a screen capture (Select, Wayland)";
+        command = writeShellScript "screen-capture-select" "grim -g \"$(slurp -d)\" - | wl-copy; dunstify \"A rect screenshot is copied!\"";
+      }
+      {
+        name = "Take a screen capture (Active Window, Hyprland)";
+        command = writeShellScript "screen-capture-window" "hyprctl -j activewindow | jq -r '\"\\(.at[0]),\\(.at[1]) \\(.size[0])x\\(.size[1])\"' | grim -g - - | wl-copy; dunstify \"A winwdow screenshot is copied!\"";
       }
     ];
   };
