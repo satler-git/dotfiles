52f71d5db750af5263d60b126e40a06f73e154cd
satler <satler@satler.dev>
2025-05-20 14:19:54 +0900

    feat: update stable branch

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
     # nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+    nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable-small.url = "github:NixOS/nixpkgs/nixos-unstable-small";
 
     # NixOS Modules
