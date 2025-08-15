cda33616e68c839398a587c08601aea7a80fc8e9
satler <satler@satler.dev>
2025-08-16 01:00:37 +0900

    feat: add yazi.nvim

--- config/nvim/lua/plugins/ui/tree.lua
+++ config/nvim/lua/plugins/ui/tree.lua
@@ -1,12 +1,39 @@
 return {
-  "stevearc/oil.nvim",
-  opts = {
-    skip_confirm_for_simple_edits = true,
+  {
+    "stevearc/oil.nvim",
+    opts = {
+      skip_confirm_for_simple_edits = true,
+    },
+    keys = {
+      { "<C-n>", "<Cmd>Oil<CR>" },
+    },
+    cmd = { "Oil" },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
   },
-  keys = {
-    { "<C-n>", "<Cmd>Oil<CR>" },
+  ---@type LazySpec
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      {
+        "<C-y>",
+        "<Cmd>Yazi toggle<CR>",
+        desc = "Resume the last yazi session",
+      },
+    },
+    ---@type YaziConfig | {}
+    opts = {
+      open_for_directories = true,
+      keymaps = {
+        show_help = "<f1>",
+      },
+      open_file_function = function(chosen_file, _, _)
+        vim.cmd(string.format("tabnew %s", vim.fn.fnameescape(chosen_file)))
+      end,
+    },
   },
-  cmd = { "Oil" },
-  -- Optional dependencies
-  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
 }
