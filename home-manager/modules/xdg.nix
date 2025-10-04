37850b41df833e3d406661b9b6d511b41d35466d
satler <satler@satler.dev>
2025-10-04 20:44:55 +0900

    home: disable hyprland xdg portal

--- home-manager/modules/xdg.nix
+++ home-manager/modules/xdg.nix
@@ -36,14 +36,14 @@
     portal = {
       enable = true;
       configPackages = with pkgs; [
-        # xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland # ここではHyprland上ではxdg-desktop-portal-hyprlandのシェアピッカーを使うという設定とその実装をそれぞれ使っている
+        xdg-desktop-portal-gtk
+        # xdg-desktop-portal-hyprland # ここではHyprland上ではxdg-desktop-portal-hyprlandのシェアピッカーを使うという設定とその実装をそれぞれ使っている
         # xdg-desktop-portal
         # firefox # これでもできるけど上のほうがbetter(普通のUnixっぽい)
       ];
       extraPortals = with pkgs; [
         xdg-desktop-portal-gtk
-        xdg-desktop-portal-hyprland
+        # xdg-desktop-portal-hyprland
         xdg-desktop-portal
       ];
       xdgOpenUsePortal = true;
