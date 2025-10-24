add2a0b1da02079a430a74ddc298992667506b91
satler <satler@satler.dev>
2025-10-24 23:17:08 +0900

    neovim:

--- config/nvim/lua/plugins/coding/edit/pair-brackets.lua
+++ config/nvim/lua/plugins/coding/edit/pair-brackets.lua
@@ -1,3 +1,24 @@
+local function par(par_tbl)
+  return function(ctx)
+    local in_par = false
+
+    local node = vim.treesitter.get_node({ pos = { ctx.row(), ctx.col() } })
+
+    if not node then
+      return true
+    end
+
+    while node do
+      if vim.tbl_contains(par_tbl, node:type()) then
+        in_par = true
+        break
+      end
+      node = node:parent()
+    end
+    return in_par
+  end
+end
+
 return {
   {
     "kylechui/nvim-surround",
@@ -18,41 +39,42 @@ return {
         insx.with({
           enabled = function(ctx)
             local before_borrowing = ctx.match([=[&\%#]=])
-            local in_type_parameters = false
-
-            local node = vim.treesitter.get_node({ pos = { ctx.row(), ctx.col() } })
-
-            if not node then
-              return true
-            end
-
-            while node do
-              -- print(node:type())
-              if
-                vim.tbl_contains({
-                  "trait_bounds",
-                  "type_arguments",
-                  "type_parameters",
-                  -- あんまりこれは関係なくて、tree-sitterの仕様上?
-                  -- A: |,
-                  -- のように,がないとtrait_bounds判定にならない
-                  -- TODO:
-                  "where_clause",
-                }, node:type())
-              then
-                in_type_parameters = true
-                break
-              end
-              node = node:parent()
-            end
-
-            -- print(before_borrowing)
-            -- print(in_type_parameters)
+            local in_type_parameters = par({
+              "trait_bounds",
+              "type_arguments",
+              "type_parameters",
+              -- あんまりこれは関係なくて、tree-sitterの仕様上?
+              -- A: |,
+              -- のように,がないとtrait_bounds判定にならない
+              -- TODO:
+              "where_clause",
+            })(ctx)
 
             return in_type_parameters or before_borrowing
           end,
           action = function(ctx)
             ctx.send("'")
+          end,
+          priority = 1000,
+        }, {
+          insx.with.filetype({ "rust" }),
+        })
+      )
+
+      insx.add(
+        ":",
+        insx.with({
+          enabled = par({
+            "trait_bounds",
+            "type_arguments",
+            "type_parameters",
+            "where_clause",
+          }),
+          action = function(ctx)
+            ctx.send(": ")
+            local row, col = ctx.row(), ctx.col()
+            ctx.send(",")
+            ctx.move(row, col)
           end,
           priority = 1000,
         }, {
