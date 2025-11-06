e213b5d47ddcb4d92b876660c858347a7211902d
satler <satler@satler.dev>
2025-11-06 23:51:56 +0900

    chore: update

--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
     };
     nixos-hardware.url = "github:NixOS/nixos-hardware";
     lanzaboote = {
-      url = "github:nix-community/lanzaboote/v0.4.2";
+      url = "github:nix-community/lanzaboote/v0.4.3";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     disko = {
