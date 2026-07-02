b7947dc95866b712fa6ca9b9f4eb279125173eed
satler <satler@satler.dev>
2026-07-02 23:12:36 +0900

    disable some systems for dank

--- hosts/modules/apps/dms-shell.nix
+++ hosts/modules/apps/dms-shell.nix
@@ -1,3 +1,4 @@
+{ lib, ... }:
 {
   programs.dms-shell = {
     enable = true;
@@ -8,4 +9,30 @@
     };
 
   };
+
+  # DMS 内蔵のロック画面 (loginctl lock-session にも反応する) を使うので hyprlock は不要
+  programs.hyprlock.enable = lib.mkForce false;
+
+  # DMS のバッテリー表示は UPower 経由
+  services.upower.enable = true;
+
+  home-manager.sharedModules = [
+    (
+      { pkgs, lib, ... }:
+      {
+        # アイドル管理・通知も DMS に任せる
+        services.swayidle.enable = lib.mkForce false;
+        services.dunst.enable = lib.mkForce false;
+
+        # dunstify は通知の送信に使うので残す
+        # (pkgs.dunst をそのまま入れると D-Bus activation で dunst が起動しうるので bin だけ)
+        home.packages = [
+          (pkgs.runCommand "dunstify" { } ''
+            mkdir -p $out/bin
+            ln -s ${pkgs.dunst}/bin/dunstify $out/bin/dunstify
+          '')
+        ];
+      }
+    )
+  ];
 }
